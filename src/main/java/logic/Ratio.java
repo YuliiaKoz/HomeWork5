f7e2fdb0005d39bb25b3ed7108de3bbb69a15d0b
Julia Kozyreva <julia_kozyreva@ukr.net>
2021-06-10 18:04:24 -0400

    added Ratio 2

--- src/main/java/logic/Ratio.java
+++ src/main/java/logic/Ratio.java
@@ -14,6 +14,7 @@ public class Ratio {
     public void Ratio2() {
     }
 
+
     public Ratio(Integer firstNumber, Integer secondNumber) {
         this.firstNumber = firstNumber;
         this.secondNumber = secondNumber;
